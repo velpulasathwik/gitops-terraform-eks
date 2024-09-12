b666185e4ec5df2f7f771ece5402d2839aa42b33
velpulasathwik <velpulasathwik93@gmail.com>
2024-09-12 22:23:06 +0530

    adding tf files to repo

---
+++ security_group.tf
@@ -0,0 +1,19 @@
+
+resource "aws_security_group" "all_worker_mgmt" {
+  name_prefix = "all_worker_management"
+  vpc_id      = module.vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "all_worker_mgmt_ingress" {
+  description       = "allow inbound traffic from eks"
+  from_port         = 0
+  protocol          = "-1"
+  to_port           = 0
+  security_group_id = aws_security_group.all_worker_mgmt.id
+  type              = "ingress"
+  cidr_blocks = [
+    "10.0.0.0/8",
+    "172.16.0.0/12",
+    "192.168.0.0/16",
+  ]
+}
